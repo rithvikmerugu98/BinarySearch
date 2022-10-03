2a50d6eaa2a6cab849f357fa6d74b74fb0f15aa2
rithvikmerugu98 <95103037+rithvikmerugu98@users.noreply.github.com>
2022-10-03 16:25:00 -0700

    setting test cases

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -5,14 +5,13 @@ public class BinarySearch {
      * This method takes a key and searches for the element in arr using binary search.
      * If the element is found then we return the index of that element
      * else we return -1.
-     *
      * In case of duplicates the program returns the value of the first occurrence of the element
      *
      * @param arr int[] - containing all the elements in sorted order
      * @param key int - the value of the key to be found.
      * @return int - index of the key in arr.
      */
-    public static int binarySearch(int[] arr, int key){
+    public static int binarySearch(int arr[], int key){
         int first = 0;
         int last=arr.length-1;
 
--- src/test/java/org/example/BinarySearchTest.java
+++ src/test/java/org/example/BinarySearchTest.java
@@ -6,70 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
     @Test
-    void testBinarySearchValidSearch() {
+    void testBinarySearchRightOnly () {
         int[] arr = new int[]{1,4,5,11,16};
-        int key = 5;
-        int index = BinarySearch.binarySearch(arr, key);
-        int expected = 2;
-        assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
-    }
-
-    @Test
-    void testBinarySearchInvalidSearch() {
-        int[] arr = new int[]{1,4,5,11,16};
-        int key = 10;
-        int index = BinarySearch.binarySearch(arr, key);
-
-        int expected = -1;
-        assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
-    }
-
-    @Test
-    void testBinarySearchEmptyArray() {
-        int[] arr = new int[]{};
-        int key = 10;
-        int index = BinarySearch.binarySearch(arr, key);
-
-        int expected = -1;
-        assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
-    }
-
-    @Test
-    void testBinarySearchSingleElement() {
-        int[] arr = new int[]{2};
-        int key = 2;
-        int index = BinarySearch.binarySearch(arr, key);
-
-        int expected = 0;
-        assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
-    }
-
-    @Test
-    void testBinarySearchNegativeValueArray() {
-        int[] arr = new int[]{-1,-4,-5,-11,16};
         int key = 16;
         int index = BinarySearch.binarySearch(arr, key);
-
         int expected = 4;
         assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
     }
 
     @Test
-    void testBinarySearchNegativeKey() {
-        int[] arr = new int[]{-1,-4,-5,11,16};
-        int key = -5;
+    void testBinarySearchInvalidRightSearch() {
+        int[] arr = new int[]{1,4,5,11,16};
+        int key = 17;
         int index = BinarySearch.binarySearch(arr, key);
 
-        int expected = 2;
+        int expected = -1;
         assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
     }
 
     @Test
-    void testBinarySearchDuplicateValues() {
-        int[] arr = new int[]{1, 2, 3, 4, 4};
+    void testBinarySearchValidLeftSearch() {
+        int[] arr = new int[]{1,4,5,11,16};
         int key = 4;
-        int index = BinarySearch.binarySearch(arr, key);
-        int expected = 3;
+        BinarySearch search = new BinarySearch();
+        int index = search.binarySearch(arr, key);
+        int expected = 1;
         assertEquals(expected, index, String.format("Expected the index to be %d but got %d", expected, index));
     }
 
